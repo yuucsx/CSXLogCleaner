706b67f33eca923f2924a1512e95cf7d95e24973
Misa <yuu.csx@gmail.com>
2025-01-19 12:39:59 -0300

    updated to better riot path and vanguard logs

--- Cleaner.cpp
+++ Cleaner.cpp
@@ -28,26 +28,19 @@ std::string expandEnvironmentVariables(const std::string& str) {
 const std::vector<std::string> PATHS = {
     "%userprofile%\\AppData\\Local\\Riot Games",
     "%ProgramData%\\Riot Games",
+    "%USERPROFILE%\\Documents\\League of Legends\\Highlights",
+    "Riot Games\\League of Legends\\Config",
     "Riot Games\\League of Legends\\Config\\Champions",
     "Riot Games\\League of Legends\\Config\\Global",
     "Riot Games\\League of Legends\\Logs",
-    "Riot Games\\League of Legends\\LCCache",
     "Riot Games\\League of Legends\\GPUCache",
+    "Riot Games\\League of Legends\\Screenshots",
+    "Riot Games\\League of Legends\\Cache",
+    "Riot Games\\League of Legends\\LCCache",
     "%ProgramFiles%\\Riot Vanguard\\Logs",
     "%ProgramFiles(x86)%\\Riot Vanguard\\Logs",
-    "%ProgramData%\\Riot Games\\machine.cfg",
-    "Riot Games\\League of Legends\\Config\\input.ini",
-    "Riot Games\\League of Legends\\Config\\ItemSets.json",
-    "Riot Games\\League of Legends\\Config\\LCUAccountPreferences.yaml",
-    "Riot Games\\League of Legends\\Config\\LCULocalPreferences.yaml",
-    "Riot Games\\League of Legends\\Config\\LeagueClientSettings.yaml",
-    "Riot Games\\League of Legends\\Config\\PerksPreferences.yaml",
-    "Riot Games\\League of Legends\\debug.log",
-    "Riot Games\\Riot Client\\UX\\natives_blob.bin",
-    "Riot Games\\Riot Client\\UX\\snapshot_blob.bin",
-    "Riot Games\\Riot Client\\UX\\v8_context_snapshot.bin",
-    "Riot Games\\Riot Client\\UX\\icudtl.dat",
-    "Riot Games\\Riot Client\\UX\\Plugins\\plugin-manifest.json"
+    "C:\\Windows\\Prefetch\\LEAGUE*.*",
+    "C:\\Windows\\Prefetch\\RIOT*.*"
 };
 
 // Drives to check
@@ -55,14 +48,8 @@ const std::vector<std::string> DRIVES = { "C", "D", "E" };
 
 void stopProcesses() {
     std::vector<std::string> processes = {
-        "LeagueClientUx.exe",
-        "League of Legends.exe",
-        "RiotClientServices.exe",
-        "RiotClientUxRender.exe",
-        "LeagueClient.exe",
-        "LeagueCrashHandler.exe",
-        "vgtray.exe",
-        "League*", "LeagueClient*", "RiotClientServices*", "riot*"
+        "LeagueClientUx.exe", "League of Legends.exe", "RiotClientServices.exe",
+        "RiotClientUxRender.exe", "LeagueClient.exe", "LeagueCrashHandler.exe"
     };
 
     setColor(COLOR_YELLOW);
@@ -77,7 +64,7 @@ void stopProcesses() {
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
 
-void cleanLogs() {
+void cleanLogs(bool deleteConfig) {
     stopProcesses();
     std::system("cls");
 
@@ -110,28 +97,20 @@ void cleanLogs() {
                 }
             }
         }
-    }
 
-    for (const auto& drive : DRIVES) {
-        std::string prefetchPath = drive + ":\\Windows\\Prefetch";
-        if (std::filesystem::exists(prefetchPath)) {
-            setColor(COLOR_YELLOW);
-            std::cout << "[-] Deleting Prefetch files in " << prefetchPath << std::endl;
-            setColor(COLOR_WHITE);
-            for (const auto& entry : std::filesystem::directory_iterator(prefetchPath)) {
-                std::string filename = entry.path().filename().string();
-                if (filename.find("LEAGUE") != std::string::npos || filename.find("RIOT") != std::string::npos) {
-                    try {
-                        std::filesystem::remove(entry.path());
-                        setColor(COLOR_CYAN);
-                        std::cout << "[-] Deleted: " << entry.path().string() << std::endl;
-                        setColor(COLOR_WHITE);
-                    }
-                    catch (const std::filesystem::filesystem_error& e) {
-                        setColor(COLOR_RED);
-                        std::cerr << "[!] Error deleting " << entry.path().string() << ": " << e.what() << std::endl;
-                        setColor(COLOR_WHITE);
-                    }
+        if (deleteConfig) {
+            std::string configPath = drive + ":\\Riot Games\\League of Legends\\Config";
+            if (std::filesystem::exists(configPath)) {
+                setColor(COLOR_YELLOW);
+                std::cout << "[-] Deleting Config files in: " << configPath << std::endl;
+                setColor(COLOR_WHITE);
+                try {
+                    std::filesystem::remove_all(configPath);
+                }
+                catch (const std::filesystem::filesystem_error& e) {
+                    setColor(COLOR_RED);
+                    std::cerr << "[!] Error deleting " << configPath << ": " << e.what() << std::endl;
+                    setColor(COLOR_WHITE);
                 }
             }
         }
@@ -139,12 +118,11 @@ void cleanLogs() {
 
     std::system("cls");
     setColor(COLOR_GREEN);
-    std::cout << "[+] All logs and caches have been cleaned. Enjoy your game!" << std::endl;
+    std::cout << "[+] Logs and caches have been cleaned." << (deleteConfig ? " Config files were also deleted." : " Config files were not deleted.") << std::endl;
     setColor(COLOR_WHITE);
 }
 
-int main()
-{
+int main() {
     SetConsoleOutputCP(CP_UTF8);
     std::system("cls");
 
@@ -163,99 +141,24 @@ int main()
     setColor(COLOR_WHITE);
 
     std::cout << "Log Cleaner for Riot Games' League of Legends to prevent automatic HWID bans." << std::endl;
-    std::cout << "This tool will delete logs and cache files related to Riot Games." << std::endl << std::endl;
 
     char choice;
-    std::cout << "Do you want to clean logs from all drives? (Y/N): ";
+    std::cout << "Do you want to delete config files? (Y/N): ";
     std::cin >> choice;
     choice = std::toupper(choice);
 
     if (choice == 'Y') {
-        cleanLogs();
-        system("PAUSE");
+        cleanLogs(true);
     }
     else if (choice == 'N') {
-        char driveLetter;
-        std::cout << "Enter the drive letter where League is installed (A-Z): ";
-        std::cin >> driveLetter;
-        driveLetter = std::toupper(driveLetter);
-
-        if (std::isalpha(driveLetter)) {
-            stopProcesses();
-            std::system("cls");
-
-            setColor(COLOR_YELLOW);
-            std::cout << "[-] Deleting all Riot/League logs and caches from drive " << driveLetter << ":" << std::endl;
-            setColor(COLOR_WHITE);
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-            std::string drive(1, driveLetter);
-            for (const auto& path : PATHS) {
-                std::string fullPath = drive + ":\\" + path;
-                fullPath = expandEnvironmentVariables(fullPath);
-
-                if (std::filesystem::exists(fullPath)) {
-                    setColor(COLOR_CYAN);
-                    std::cout << "[-] Deleting: " << fullPath << std::endl;
-                    setColor(COLOR_WHITE);
-                    try {
-                        if (std::filesystem::is_directory(fullPath)) {
-                            std::filesystem::remove_all(fullPath);
-                        }
-                        else {
-                            std::filesystem::remove(fullPath);
-                        }
-                    }
-                    catch (const std::filesystem::filesystem_error& e) {
-                        setColor(COLOR_RED);
-                        std::cerr << "[!] Error deleting " << fullPath << ": " << e.what() << std::endl;
-                        setColor(COLOR_WHITE);
-                    }
-                }
-            }
-
-            std::string prefetchPath = drive + ":\\Windows\\Prefetch";
-            if (std::filesystem::exists(prefetchPath)) {
-                setColor(COLOR_YELLOW);
-                std::cout << "[-] Deleting Prefetch files in " << prefetchPath << std::endl;
-                setColor(COLOR_WHITE);
-                for (const auto& entry : std::filesystem::directory_iterator(prefetchPath)) {
-                    std::string filename = entry.path().filename().string();
-                    if (filename.find("LEAGUE") != std::string::npos || filename.find("RIOT") != std::string::npos) {
-                        try {
-                            std::filesystem::remove(entry.path());
-                            setColor(COLOR_CYAN);
-                            std::cout << "[-] Deleted: " << entry.path().string() << std::endl;
-                            setColor(COLOR_WHITE);
-                        }
-                        catch (const std::filesystem::filesystem_error& e) {
-                            setColor(COLOR_RED);
-                            std::cerr << "[!] Error deleting " << entry.path().string() << ": " << e.what() << std::endl;
-                            setColor(COLOR_WHITE);
-                        }
-                    }
-                }
-            }
-
-            std::system("cls");
-            setColor(COLOR_GREEN);
-            std::cout << "[+] All logs and caches have been cleaned from drive " << driveLetter << ":. Enjoy your game!" << std::endl;
-            setColor(COLOR_WHITE);
-            system("PAUSE");
-        }
-        else {
-            setColor(COLOR_RED);
-            std::cout << "[!] Invalid drive letter. Please enter a letter (A-Z)." << std::endl;
-            setColor(COLOR_WHITE);
-            system("PAUSE");
-        }
+        cleanLogs(false);
     }
     else {
         setColor(COLOR_RED);
         std::cout << "[!] Invalid option. Exiting..." << std::endl;
         setColor(COLOR_WHITE);
-        system("PAUSE");
     }
 
+    system("PAUSE");
     return 0;
 }
